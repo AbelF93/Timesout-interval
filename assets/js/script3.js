811dd4c31f268ed0e514b076dadd1cc13d0cc325
AbelF93 <abel.fortunati@gmail.com>
2023-05-02 15:55:52 +0200

    desktop design

---
+++ assets/js/script3.js
@@ -0,0 +1,43 @@
+/* Exercice 3:
+ * Create a Whack-a-Mole Game
+ * Each seconds, a new mole appear
+ * Clicking the mole add a point to the score 
+*/
+const garden = document.querySelector("body");
+
+
+garden.innerHTML =` <section>
+                    <div class="mole-hole">
+                    <button class="mole__button"></button>
+                    </div>
+
+                    <div class="mole-hole">
+                    <button class="mole__button"></button>
+                    </div>
+
+                    <div class="mole-hole">
+                    <button class="mole__button"></button>
+                    </div>
+                    
+                    <div class="mole-hole">
+                    <button class="mole__button"></button>
+                    </div>
+                    
+                    <div class="mole-hole">
+                    <button class="mole__button"></button>
+                    </div>
+                    
+                    <div class="mole-hole">
+                    <button class="mole__button"></button>
+                    </div>
+
+                    <div class="mole-hole">
+                    <button class="mole__button"></button>
+                    </div>
+
+                    <div class="mole-hole">
+                    <button class="mole__button"></button>
+                    </div>
+                    </section>
+                   `;
+
